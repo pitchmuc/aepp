b8307abae04ffe4fc4f06996586983279199031f
Julien Piccini <piccini.julien@gmail.com>
2020-12-21 17:41:38 +0100

    adding configure method to setup the aepp connection

--- aepp/__init__.py
+++ aepp/__init__.py
@@ -3,60 +3,9 @@
 from aepp import modules
 from aepp import config
 from aepp import connector
-from .__version__ import *
+from .configs import *
+from .__version__ import __version__
 connection = None
-
-def createConfigFile(sandbox: bool = False, verbose: object = False, **kwargs)->None:
-    """
-    This function will create a 'config_admin.json' file where you can store your access data.
-    Arguments:
-        sandbox : OPTIONAL : consider to add a parameter for sandboxes
-        verbose : OPTIONAL : set to true, gives you a print stateent where is the location.
-    Possible kwargs:
-        filename : name of the config file.
-    """
-    json_data = {
-        'org_id': '<orgID>',
-        'client_id': "<client_id>",
-        'tech_id': "<something>@techacct.adobe.com",
-        'secret': "<YourSecret>",
-        'pathToKey': '<path/to/your/privatekey.key>',
-    }
-    if sandbox:
-        json_data['sandbox-name'] = "<your_sandbox_name>"
-    filename = f"{kwargs.get('filename', 'config_aep')}.json"
-    with open(filename, 'w') as cf:
-        cf.write(modules.json.dumps(json_data, indent=4))
-    if verbose:
-        print(f'File created at this location :{config._cwd}/{filename}')
-
-
-def importConfigFile(file: str)-> None:
-    """
-    This function will read the 'config_admin.json' to retrieve the information to be used by this module.
-    """
-    with open(modules.Path(file), 'r') as file:
-        f = modules.json.load(file)
-        config.config_object["org_id"] = f['org_id']
-        config.header["x-gw-ims-org-id"] = config.config_object["org_id"]
-        if 'api_key' in f.keys():
-            config.config_object["client_id"] = f['api_key']
-        elif 'client_id' in f.keys():
-            config.config_object["client_id"] = f['client_id']
-        config.header["x-api-key"] = config.config_object["client_id"] 
-        config.header['Authorization'] = ''
-        config.config_object["tech_id"] = f['tech_id']
-        config.config_object["secret"] = f['secret']
-        config.config_object["pathToKey"] = f['pathToKey']
-        config.config_object["token"] = ""
-        config.date_limit = 0
-        if 'sandbox-name' in f.keys():
-            config.header["x-sandbox-name"] = f['sandbox-name']
-            config.config_object["sandbox"] = f['sandbox-name']
-        else:
-            config.header["x-sandbox-name"] = "prod"
-            config.config_object["sandbox"] = "prod"
-
 
 def home(product: str = None, limit: int = 50):
     """
---
+++ aepp/configs.py
@@ -0,0 +1,160 @@
+import json
+import os
+from pathlib import Path
+from typing import Optional
+import json
+
+# Non standard libraries
+from .config import config_object, header
+
+def find_path(path: str) -> Optional[Path]:
+    """Checks if the file denoted by the specified `path` exists and returns the Path object
+    for the file.
+
+    If the file under the `path` does not exist and the path denotes an absolute path, tries
+    to find the file by converting the absolute path to a relative path.
+
+    If the file does not exist with either the absolute and the relative path, returns `None`.
+    """
+    if Path(path).exists():
+        return Path(path)
+    elif path.startswith('/') and Path('.' + path).exists():
+        return Path('.' + path)
+    elif path.startswith('\\') and Path('.' + path).exists():
+        return Path('.' + path)
+    else:
+        return None
+
+def createConfigFile(destination: str = 'config_aep_template.json',sandbox: str = 'prod', verbose: object = False, **kwargs)->None:
+    """
+    This function will create a 'config_admin.json' file where you can store your access data.
+    Arguments:
+        destination : OPTIONAL : if you wish to save the file at a specific location.
+        sandbox : OPTIONAL : You can directly set your sandbox name in this parameter.
+        verbose : OPTIONAL : set to true, gives you a print stateent where is the location.
+    """
+    json_data:dict = {
+        'org_id': '<orgID>',
+        'client_id': "<client_id>",
+        'tech_id': "<something>@techacct.adobe.com",
+        'secret': "<YourSecret>",
+        'pathToKey': '<path/to/your/privatekey.key>',
+        'sandbox-name' : sandbox
+    }
+    if '.json' not in destination:
+        destination:str = f"{destination}.json"
+    with open(destination, 'w') as cf:
+        cf.write(json.dumps(json_data, indent=4))
+    if verbose:
+        print(
+            f" file created at this location : {os.getcwd()}{os.sep}config_analytics.json"
+        )
+
+
+def importConfigFile(path: str) -> None:
+    """Reads the file denoted by the supplied `path` and retrieves the configuration information
+    from it.
+
+    Arguments:
+        path: REQUIRED : path to the configuration file. Can be either a fully-qualified or relative.
+
+    Example of path value.
+    "config.json"
+    "./config.json"
+    "/my-folder/config.json"
+    """
+
+    import_config_file(path)
+
+
+def import_config_file(path: str) -> None:
+    """Reads the file denoted by the supplied `path` and retrieves the configuration information
+    from it.
+
+    Arguments:
+        path: REQUIRED : path to the configuration file. Can be either a fully-qualified or relative.
+
+    Example of path value.
+    "config.json"
+    "./config.json"
+    "/my-folder/config.json"
+    """
+
+    config_file_path: Optional[Path] = find_path(path)
+    if config_file_path is None:
+        raise FileNotFoundError(
+            f"Unable to find the configuration file under path `{path}`."
+        )
+    with open(config_file_path, 'r') as file:
+        provided_config = json.load(file)
+        provided_keys = provided_config.keys()
+        if 'api_key' in provided_keys:
+            ## old naming for client_id
+            client_id = provided_config['api_key']
+        elif 'client_id' in provided_keys:
+            client_id = provided_config['client_id']
+        else:
+            raise RuntimeError(f"Either an `api_key` or a `client_id` should be provided.")
+        configure(
+            org_id=provided_config['org_id'],
+            tech_id=provided_config['tech_id'],
+            secret=provided_config['secret'],
+            path_to_key=provided_config['pathToKey'],
+            client_id=client_id
+        )
+
+
+def configure(org_id: str = None,
+              tech_id: str = None,
+              secret: str = None,
+              client_id: str = None,
+              path_to_key: str=None,
+              private_key: str = None,
+              sandbox:str="prod",
+              ):
+    """Performs programmatic configuration of the API using provided values.
+    Arguments:
+        org_id : REQUIRED : Organization ID
+        tech_id : REQUIRED : Technical Account ID
+        secret : REQUIRED : secret generated for your connection
+        client_id : REQUIRED : The client_id (old api_key) provided by the JWT connection. 
+        path_to_key : REQUIRED : If you have a file containing your private key value.
+        private_key : REQUIRED : If you do not use a file but pass a variable directly.
+        sandbox : OPTIONAL : If not provided, default to prod
+    """
+    if not org_id:
+        raise ValueError("`org_id` must be specified in the configuration.")
+    if not client_id:
+        raise ValueError("`client_id` must be specified in the configuration.")
+    if not tech_id:
+        raise ValueError("`tech_id` must be specified in the configuration.")
+    if not secret:
+        raise ValueError("`secret` must be specified in the configuration.")
+    if not path_to_key and not private_key:
+        raise ValueError("`pathToKey` or `private_key` must be specified in the configuration.")
+    config_object["org_id"] = org_id
+    config_object["client_id"] = client_id
+    header["x-api-key"] = client_id
+    config_object["tech_id"] = tech_id
+    config_object["secret"] = secret
+    config_object["pathToKey"] = path_to_key
+    config_object["private_key"] = private_key
+    config_object['sandbox'] = sandbox
+    # ensure the reset of the state by overwriting possible values from previous import.
+    config_object["date_limit"] = 0
+    config_object["token"] = ""
+
+
+def get_private_key_from_config(config: dict) -> str:
+    """
+    Returns the private key directly or read a file to return the private key.
+    """
+    private_key = config.get('private_key')
+    if private_key is not None:
+        return private_key
+    private_key_path = find_path(config['pathToKey'])
+    if private_key_path is None:
+        raise FileNotFoundError(f'Unable to find the private key under path `{config["pathToKey"]}`.')
+    with open(Path(private_key_path), 'r') as f:
+        private_key = f.read()
+    return private_key
