9bd63f1942890337904d3a93535ab774766b1dd6
Julien Piccini <piccini.julien@gmail.com>
2021-01-26 21:18:20 +0100

    removing modules usage in connector

--- aepp/connector.py
+++ aepp/connector.py
@@ -1,5 +1,15 @@
-from aepp import config
-from aepp import modules
+## Internal modules
+from aepp import config, configs
+
+## External module
+import json
+import os
+from typing import Dict, Optional, Union
+from copy import deepcopy
+import time
+import requests
+from pathlib import Path
+import jwt
 
 class AdobeRequest:
     """
@@ -18,16 +28,22 @@ class AdobeRequest:
         if config_object['org_id'] == "":
             raise Exception(
                 'You have to upload the configuration file with importConfigFile method.')
-        self.config = modules.deepcopy(config_object)
-        self.header = modules.deepcopy(header)
+        self.config = deepcopy(config_object)
+        self.header = deepcopy(header)
         self.retry = retry
-        if self.config['token'] == "" or modules.time.time() > self.config['date_limit']:
+        if self.config['token'] == "" or time.time() > self.config['date_limit']:
             if 'aepScope' in kwargs.keys() and "privacyScope" in kwargs.keys():
-                self.token = self.retrieveToken(verbose=verbose,aepScope=kwargs.get('aepScope'),privacyScope=kwargs.get('privacyScope'))
+                token_with_expiry = self.get_token_and_expiry_for_config(config=self.config, verbose=verbose,aepScope=kwargs.get('aepScope'),privacyScope=kwargs.get('privacyScope'))
+                self.token = token_with_expiry['token']
+                self.config['date_limit'] = time.time() + token_with_expiry['expiry'] / 1000 - 500
+                self.header.update({'Authorization': f'Bearer {self.token}'})
             else:
-                self.token = self.retrieveToken(verbose=verbose)
+                token_with_expiry = self.get_token_and_expiry_for_config(config=self.config, verbose=verbose,aepScope=kwargs.get('aepScope'),privacyScope=kwargs.get('privacyScope'))
+                self.token = token_with_expiry['token']
+                self.config['date_limit'] = time.time() + token_with_expiry['expiry'] / 1000 - 500
+                self.header.update({'Authorization': f'Bearer {self.token}'})
     
-    def _find_path(self,path: str) -> modules.Optional[modules.Path]:
+    def _find_path(self,path: str) -> Optional[Path]:
         """Checks if the file denoted by the specified `path` exists and returns the Path object
         for the file.
 
@@ -36,83 +52,84 @@ class AdobeRequest:
 
         If the file does not exist with either the absolute and the relative path, returns `None`.
         """
-        if modules.Path(path).exists():
-            return modules.Path(path)
-        elif path.startswith('/') and modules.Path('.' + path).exists():
-            return modules.Path('.' + path)
-        elif path.startswith('\\') and modules.Path('.' + path).exists():
-            return modules.Path('.' + path)
+        if Path(path).exists():
+            return Path(path)
+        elif path.startswith('/') and Path('.' + path).exists():
+            return Path('.' + path)
+        elif path.startswith('\\') and Path('.' + path).exists():
+            return Path('.' + path)
         else:
             return None
 
-    def retrieveToken(self, verbose: bool = False, **kwargs)->str:
-        """ Retrieve the token by using the information provided by the user during the import importConfigFile function. 
-        Argument : 
-            verbose : OPTIONAL : Default False. If set to True, print information.
-        kwargs:
-            privacyScope : if set to True, add the Privacy scope into the token request.
-            aepScope : if set to False, remove the AEP JWT scope from token request.
+    def get_token_and_expiry_for_config(self, config: dict, verbose: bool = False, save: bool = False, **kwargs) ->Dict[str, str]:
         """
-        private_key_path = self._find_path(self.config['pathToKey'])
-        if private_key_path is None:
-            raise FileNotFoundError(
-                f"Unable to find the configuration file under path `{self.config['pathToKey']}`.")
-        with open(modules.Path(private_key_path), 'r') as f:
-            private_key_unencrypted = f.read()
-            header_jwt = {'cache-control': 'no-cache',
-                          'content-type': 'application/x-www-form-urlencoded'}
-        jwtPayload = {
-            # Expiration set to 24 hours
-            "exp": round(24*60*60 + int(modules.time.time())),
-            "iss": self.config['org_id'],  # org_id
-            # technical_account_id
-            "sub": self.config['tech_id'],
+        Retrieve the token by using the information provided by the user during the import importConfigFile function.
+        ArgumentS :
+            verbose : OPTIONAL : Default False. If set to True, print information.
+            save : OPTIONAL : Default False. If set to True, save the toke in the .
+        """
+        private_key = configs.get_private_key_from_config(config)
+        header_jwt = {
+            'cache-control': 'no-cache',
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+        now_plus_24h = int(time.time()) + 24 * 60 * 60
+        jwt_payload = {
+            'exp': now_plus_24h,
+            'iss': config['org_id'],
+            'sub': config['tech_id'],
             "https://ims-na1.adobelogin.com/s/ent_dataservices_sdk": True,
-            "aud": "https://ims-na1.adobelogin.com/c/"+self.config['client_id']
+            'aud': f'https://ims-na1.adobelogin.com/c/{config["client_id"]}'
         }
         #privacy topic
         if kwargs.get("privacyScope", False):
-            jwtPayload["https://ims-na1.adobelogin.com/s/ent_gdpr_sdk"] = True
+            jwt_payload["https://ims-na1.adobelogin.com/s/ent_gdpr_sdk"] = True
         if kwargs.get("aepScope", True)==False:
-            del jwtPayload["https://ims-na1.adobelogin.com/s/ent_dataservices_sdk"]
-        if verbose:
-            print('JWT Payload')
-            print(jwtPayload)
-        encoded_jwt = modules.jwt.encode(
-            jwtPayload, private_key_unencrypted, algorithm='RS256')  # working algorithm
+            del jwt_payload["https://ims-na1.adobelogin.com/s/ent_dataservices_sdk"]
+        encoded_jwt = self._get_jwt(payload=jwt_payload, private_key=private_key)
+
         payload = {
-            "client_id": self.config['client_id'],
-            "client_secret": self.config['secret'],
-            "jwt_token": encoded_jwt.decode("utf-8")
+            'client_id': config['client_id'],
+            'client_secret': config['secret'],
+            'jwt_token': encoded_jwt
         }
-        response = modules.requests.post(self.config['tokenEndpoint'],
-                                              headers=header_jwt, data=payload)
+        response = requests.post(config['tokenEndpoint'], headers=header_jwt, data=payload)
         json_response = response.json()
         try:
             token = json_response['access_token']
-        except:
-            print("Issue retrieving token")
+        except KeyError:
+            print('Issue retrieving token')
             print(json_response)
-        self.config['token'] = token
-        self.header.update(
-            {"Authorization": f"Bearer {token}"})
-        expire = json_response['expires_in']
-        self.config["date_limit"] = modules.time.time() + expire/1000 - \
-            500  # end of time for the token
+        expiry = json_response['expires_in'] -500
+        if save:
+            with open('token.txt', 'w') as f:
+                f.write(token)
+            print(f'token has been saved here: {os.getcwd()}{os.sep}token.txt')
         if verbose:
-            print('token valid till : ' +
-                  modules.time.ctime(modules.time.time() + expire/1000))
-            print('token has been saved here : ' +
-                  modules.Path.as_posix(modules.Path.cwd()))
+            print('token valid till : ' + time.ctime(time.time() + expiry / 1000))
+        return {'token': token, 'expiry': expiry}
+
+
+    def _get_jwt(self,payload: dict, private_key: str) -> str:
+        """
+        Ensure that jwt enconding return the same type (str) as versions < 2.0.0 returned bytes and >2.0.0 return strings. 
+        """
+        token: Union[str, bytes] = jwt.encode(payload, private_key, algorithm='RS256')
+        if isinstance(token, bytes):
+            return token.decode('utf-8')
         return token
 
     def _checkingDate(self)->None:
         """
         Checking if the token is still valid
         """
-        now = modules.time.time()
+        now = time.time()
         if now > self.config['date_limit']:
-            self.retrieveToken()
+            token_with_expiry = self.get_token_and_expiry_for_config(config=self.config)
+            token = token_with_expiry['token']
+            self.config['token'] = token
+            self.header.update({'Authorization': f'Bearer {token}'})
+            self.config['date_limit'] = time.time() + token_with_expiry['expiry'] / 1000 - 500
     
     def updateSandbox(self,sandbox:str)->None:
         """
@@ -133,16 +150,16 @@ class AdobeRequest:
         if headers is None:
             headers = self.header
         if params == None and data == None:
-            res = modules.requests.get(
+            res = requests.get(
                 endpoint, headers=headers)
         elif params != None and data == None:
-            res = modules.requests.get(
+            res = requests.get(
                 endpoint, headers=headers, params=params)
         elif params == None and data != None:
-            res = modules.requests.get(
+            res = requests.get(
                 endpoint, headers=headers, data=data)
         elif params != None and data != None:
-            res = modules.requests.get(endpoint, headers=headers,
+            res = requests.get(endpoint, headers=headers,
                                             params=params, data=data)
         try:
             res_json = res.json()
@@ -153,7 +170,7 @@ class AdobeRequest:
             if self.retry > 0:
                 for each in range(self.retry):
                     if 'error' in res_json.keys():
-                        modules.time.sleep(5)
+                        time.sleep(5)
                         res_json = self.getData(endpoint,params,data,headers,**kwargs)
         return res_json
 
@@ -165,17 +182,17 @@ class AdobeRequest:
         if headers is None:
             headers = self.header
         if params == None and data == None:
-            res = modules.requests.post(
+            res = requests.post(
                 endpoint, headers=headers)
         elif params != None and data == None:
-            res = modules.requests.post(
+            res = requests.post(
                 endpoint, headers=headers, params=params)
         elif params == None and data != None:
-            res = modules.requests.post(endpoint, headers=headers,
-                                             data=modules.json.dumps(data))
+            res = requests.post(endpoint, headers=headers,
+                                             data=json.dumps(data))
         elif params != None and data != None:
-            res = modules.requests.post(endpoint, headers=headers,
-                                             params=params, data=modules.json.dumps(data))
+            res = requests.post(endpoint, headers=headers,
+                                             params=params, data=json.dumps(data))
         try:
             res_json = res.json()
         except:
@@ -192,14 +209,14 @@ class AdobeRequest:
         if headers is None:
             headers = self.header
         if params != None and data == None:
-            res = modules.requests.patch(
+            res = requests.patch(
                 endpoint, headers=headers, params=params)
         elif params == None and data != None:
-            res = modules.requests.patch(endpoint, headers=headers,
-                                              data=modules.json.dumps(data))
+            res = requests.patch(endpoint, headers=headers,
+                                              data=json.dumps(data))
         elif params != None and data != None:
-            res = modules.requests.patch(endpoint, headers=headers,
-                                              params=params, data=modules.json.dumps(data=data))
+            res = requests.patch(endpoint, headers=headers,
+                                              params=params, data=json.dumps(data=data))
         try:
             status_code = res.json()
         except:
@@ -216,14 +233,14 @@ class AdobeRequest:
         if headers is None:
             headers = self.header
         if params != None and data == None:
-            res = modules.requests.put(
+            res = requests.put(
                 endpoint, headers=headers, params=params)
         elif params == None and data != None:
-            res = modules.requests.put(endpoint, headers=headers,
-                                            data=modules.json.dumps(data))
+            res = requests.put(endpoint, headers=headers,
+                                            data=json.dumps(data))
         elif params != None and data != None:
-            res = modules.requests.put(endpoint, headers=headers,
-                                            params=params, data=modules.json.dumps(data=data))
+            res = requests.put(endpoint, headers=headers,
+                                            params=params, data=json.dumps(data=data))
         try:
             status_code = res.json()
         except:
@@ -240,10 +257,10 @@ class AdobeRequest:
         if headers is None:
             headers = self.header
         if params == None:
-            res = modules.requests.delete(
+            res = requests.delete(
                 endpoint, headers=headers)
         elif params != None:
-            res = modules.requests.delete(
+            res = requests.delete(
                 endpoint, headers=headers, params=params)
         try:
             status_code = res.status_code
