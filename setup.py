2a7a12b0fcbd7b7641b619d3fffb73c530dac1bd
Julien Piccini <piccini.julien@gmail.com>
2021-06-01 13:44:02 +0200

    changing setup

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pitchmuc/aep",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
