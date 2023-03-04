820dec39e98c44b77fefa7f3376bd4ecd90b22ff
Saikat Kanjilal <sxk1969@hotmail.com>
2023-03-04 12:01:44 -0800

    initial stubs and some schema tests completed

---
+++ tests/dataaccess_test.py
@@ -0,0 +1,26 @@
+from aepp.schema import Schema
+import unittest
+from unittest.mock import patch, MagicMock
+
+class DataAccessTest(unittest.TestCase):
+
+    def test_dataaccess_get_batch_files(self):
+        assert True
+
+    def test_dataaccess_get_batch_failed(self):
+        assert True
+
+    def test_dataaccess_get_batch_meta(self):
+        assert True
+
+    def test_dataaccess_getHeadFile(self):
+        assert True
+
+    def test_dataaccess_get_files(self):
+        assert True
+
+    def test_dataaccess_get_preview(self):
+        assert True
+
+    def test_dataaccess_get_resource(self):
+        assert True
