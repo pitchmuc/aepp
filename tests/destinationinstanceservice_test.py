6b091a345c19e88e3ae4cf113b35ff7444ad8101
amychen <lchen@adobe.com>
2023-03-13 12:43:11 -0700

    PLAT-145917 review comments

--- tests/destinationinstanceservice_test.py
+++ tests/destinationinstanceservice_test.py
@@ -4,13 +4,14 @@ from unittest.mock import patch, MagicMock, ANY
 
 
 class DestinationInstanceServiceTest(unittest.TestCase):
-
+    ADHOC_INPUT = {"flow1": ["dataset1"], "flow2": ["dataset2", "dataset3"]}
+    
     @patch("aepp.connector.AdobeRequest")
     def test_create_adhoc_dataset_export(self, mock_connector):
         instance_conn = mock_connector.return_value
         instance_conn.postData.return_value = {'foo'}
         destination_instance_service_obj = DestinationInstanceService()
-        result = destination_instance_service_obj.createAdHocDatasetExport({"flow1": ["dataset1"], "flow2": ["dataset2", "dataset3"]})
+        result = destination_instance_service_obj.createAdHocDatasetExport(self.ADHOC_INPUT)
         assert(result is not None)
         instance_conn.postData.assert_called_once()
         instance_conn.postData.assert_called_with(ANY, data={'activationInfo': {'destinations': [{'flowId': 'flow1', 'datasets': [{'id': 'dataset1'}]}, {'flowId': 'flow2', 'datasets': [{'id': 'dataset2'}, {'id': 'dataset3'}]}]}})
@@ -18,8 +19,7 @@ class DestinationInstanceServiceTest(unittest.TestCase):
     @patch("aepp.connector.AdobeRequest")
     def test_create_adhoc_dataset_export_invalid_input(self, mock_connector):
         destination_instance_service_obj = DestinationInstanceService()
-        try:
+        with self.assertRaises(Exception) as cm:
             destination_instance_service_obj.createAdHocDatasetExport(None)
-        except Exception as e:
-            self.assertEqual(str(e), "Require a dict for defining the flowId to datasetIds mapping")
+        self.assertEqual('Require a dict for defining the flowId to datasetIds mapping', str(cm.exception))
     
